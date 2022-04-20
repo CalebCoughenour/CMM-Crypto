9ae71064fc61c466a3090d4e4051e6b7d4535053
Caleb Coughenour <CalebCoughenour@protonmail.com>
2022-04-20 10:58:41 -0700

    display eth and btc price with btc logo
    Co-authored-by: Mike Wright <m.wright92@icloud.com>
    Co-authored-by: Mark McConnell <mark.programming1@gmail.com>

--- src/index.js
+++ src/index.js
@@ -4,13 +4,20 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import CryptoSomething from './js/ticker-api.js';
 
-
-$('#crypto-button').click(function () {
-  let promise = CryptoSomething.getPrices();
+$(document).ready(function() {
+ let promise = CryptoSomething.getPrices();
   promise.then(function(response) {
     const body = JSON.parse(response);
-    // let info = body.data
-    console.log(body);
+    let src = body[0].logo_url;
+    console.log(src);
+    $('#btc-logo').html("<img src='" + src + "'>");
+    $('#btc-display').text(`BTC price: ${body[0].price}`);
+    $('#eth-display').text(`ETH price: ${body[1].price}`);
+    
+  $('#crypto-button').click(function () {
+  
+ 
     
   });
 });
+})
