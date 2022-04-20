2109432f863f688f3434526f13a6838eb5fe7ec8
Caleb Coughenour <CalebCoughenour@protonmail.com>
2022-04-20 14:48:36 -0700

    continue styling header with animated title
    Co-authored-by: Mike Wright <m.wright92@icloud.com>
    Co-authored-by: Mark McConnell <mark.programming1@gmail.com>

--- src/index.js
+++ src/index.js
@@ -17,23 +17,18 @@ $(document).ready(function() {
     let p2 = parseFloat(price2).toFixed(4);
     let price3 = body[3].price;
     let p3 = parseFloat(price3).toFixed(2);
-    let price4 = body[4].price;
-    let p4 = parseFloat(price4).toFixed(4);
     let src = body[0].logo_url;
     let src1 = body[1].logo_url;
     let src2 = body[2].logo_url;
     let src3 = body[3].logo_url;
-    let src4 = body[4].logo_url;
     $('#btc-logo').html("<img src='" + src + "'>");
     $('#eth-logo').html("<img src='" + src1 + "'>");
     $('#USDT-logo').html("<img src='" + src2 + "'>");
     $('#BNB-logo').html("<img src='" + src3 + "'>");
-    $('#usdc-logo').html("<img src='" + src4 + "'>");
     $('#btc-display').text(`BTC price: $${p}`);
     $('#eth-display').text(`ETH price: $${p1}`);
     $('#USDT-display').text(`USDT price: $${p2}`);
     $('#BNB-display').text(`BNB price: $${p3}`);
-    $('#usdc-display').text(`USDC price: $${p4}`);
     }, function(error) {
       $('#show-errors').text(`There was an error processing your request: ${error}`);
     }
